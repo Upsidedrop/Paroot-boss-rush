365396d2fcdba5b89d34bb2cbb464a81c4eb5d3b
Upsidedrop <96622429+Upsidedrop@users.noreply.github.com>
2024-03-30 10:47:49 -0400

    1.00.09
    Started working on monkey enraged, added test build

--- Paroot takeover/Assets/Animator/Monkey/Monkey_Walk.cs
+++ Paroot takeover/Assets/Animator/Monkey/Monkey_Walk.cs
@@ -4,7 +4,8 @@ public class Monkey_Walk : StateMachineBehaviour
 {
     private Transform playerTransform;
     private Rigidbody2D rb;
-    private readonly float speed = 1;
+    public float speed = 1;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
